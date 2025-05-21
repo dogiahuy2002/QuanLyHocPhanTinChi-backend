4e20f9f74f550f2a29ba62f843e18c1b14007800
DoGiaHuy <dogiahuy1000@gmail.com>
2025-05-21 21:22:40 +0700

    fix schedule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
-import { Module, forwardRef } from '@nestjs/common'
-import { CommonModule } from '../common/common.module'
-import { PrismaModule } from '../prisma/prisma.module'
-import { UserModule } from '../user/user.module'
-import { AuthService } from './auth.service'
+import { Module, forwardRef } from "@nestjs/common";
+import { CommonModule } from "../common/common.module";
+import { PrismaModule } from "../prisma/prisma.module";
+import { UserModule } from "../user/user.module";
+import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthGuard } from "./guard/auth.guard";
+
 @Module({
-  providers: [AuthService],
-  imports: [forwardRef(() => UserModule), PrismaModule, CommonModule],
-  exports: [AuthService],
+  imports: [
+    forwardRef(() => UserModule),
+    PrismaModule,
+    CommonModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || "default_secret", // đổi secret thật trong .env
+      signOptions: { expiresIn: "1d" },
+    }),
+  ],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthService, AuthGuard, JwtModule],
 })
 export class AuthModule {}
